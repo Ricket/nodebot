eae9ce67bde8919434a29c924e8b724b027b31a2
Ricket <richard@richardcarter.org>
2011-09-07 20:53:02 -0400

    Implemented tell.js

--- scripts/tell.js
+++ scripts/tell.js
@@ -5,13 +5,44 @@
 //     ~tell someone something - Saves the message to tell the person when they get back
 //     On user join, looks to see if the new person has any saved messages and if so, says them
 
-listen(/PRIVMSG ([^ ]+) :~tell ([^ ]+) (.+)$/i, function(match) {
-	// save message into the tell folder
-	// irc.privmsg(match[1], "I'll tell them when they get back.");
+var messages;
+try {
+        messages = fs.readFileSync('data/tell.txt', 'ascii');
+        if(!messages) messages = "";
+        messages = messages.split('\n');
+} catch(err) {messages = [];}
+
+function addMessage(room, user, message) {
+        messages.push(room+"#"+user+": "+message);
+
+        fs.writeFileSync('data/tell.txt', messages.join('\n'), 'ascii');
+}
+
+function getMessages(room, user) {
+        var userMessages = [];
+		var searchString = room+"#"+user+": ";
+        for(var i=0; i<messages.length; i++) {
+                var idx = messages[i].toLowerCase().indexOf(searchString.toLowerCase());
+                if(idx == 0) {
+                        userMessages.push(messages[i].substr(searchString.length));
+                        messages.splice(i, 1);
+                        i--;
+                }
+        }
+        return userMessages;
+}
+
+listen(/:([^!]+)!.*PRIVMSG ([^ ]+) :~tell ([^ ]+) (.+)$/i, function(match) {
+        addMessage(match[2], match[3], match[1]+" told me to tell you: "+match[4]);
+
+	irc.privmsg(match[2], "I'll tell them when they get back.");
 });
 
 // listen for join
 listen(/:([^!]+)!.*JOIN :(.*)$/i, function(match) {
 	// search tell folder for any messages to give
-	// irc.privmsg(match[2], match[1]+": Ricket says hi");
+        var userMessages = getMessages(match[2], match[1]);
+        for(var i=0; i<userMessages.length; i++) {
+                irc.privmsg(match[2], match[1]+": "+userMessages[i]);
+        }
 });
