6a416daa0ec16920bc3e7fb06431a48c5dc61b93
Ricket <richard@richardcarter.org>
2011-09-08 10:06:27 -0400

    Blacklisted the word "for" in britishenglishpolice.js

--- scripts/britishenglishpolice.js
+++ scripts/britishenglishpolice.js
@@ -5,5 +5,7 @@
 //     It replaces the words ending in -or like humor, labor, favor with it's British English counterparts.
 
 listen(/PRIVMSG [^ ]+ :(.*)([a-zA-Z]+)or(\.| |\t|\n)(.*)?/i, function(match, data, replyTo) {
-	irc.privmsg(replyTo,  match[1]+match[2]+"our*"+match[3]+match[4]);
+	if(match[2] != 'f') {
+		irc.privmsg(replyTo,  match[1]+match[2]+"our*"+match[3]+match[4]);
+	}
 });
