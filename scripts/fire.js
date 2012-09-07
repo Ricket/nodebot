80c54ad68d4fe69579216654abe3d1aeed41cca5
Stephen Wright <mario.luigi.star@gmail.com>
2012-09-07 15:56:44 -0400

    Fire!
    Added fire script. It is purely fun!

---
+++ scripts/fire.js
@@ -0,0 +1,19 @@
+var OBJECTS = ["pants", "house", "cat", "mouse"],
+    object_on_fire = null;
+
+listen(/^:([^!]+)!.*PRIVMSG [^ ]+ :~fire$/i, function(match, data, replyTo) {
+    if (!object_on_fire) {
+        object_on_fire = OBJECTS[Math.floor(Math.random() * OBJECTS.length)];
+    }
+    irc.action(replyTo, "HELP!!! My " + object_on_fire + " is on fire!!! Please ~douse it!");
+});
+
+listen(/^:([^!]+)!.*PRIVMSG [^ ]+ :~douse$/i, function(match, data, replyTo) {
+    if (object_on_fire) {
+        irc.action(replyTo, "Thank you for saving my " + object_on_fire + "!");
+        object_on_fire = null;
+    }
+    else {
+        irc.action(replyTo, "You got my stuff wet; why'd you do that?!");
+    }
+});
