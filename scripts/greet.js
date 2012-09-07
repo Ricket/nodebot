501dc5b98d27458ff74361a0434f5dcdb651a011
Stephen Wright <mario.luigi.star@gmail.com>
2012-09-07 15:54:57 -0400

    greet: Throttle time between greetings

--- scripts/greet.js
+++ scripts/greet.js
@@ -2,12 +2,23 @@
 // This code is licensed under the MIT license; see LICENSE.txt for details.
 
 // This script handles the following functions:
-//     Looks for "hello", "hi", "howdy", "hola" and says hello; intended to join in on greeting someone when someone else greets them.
+//     Looks for "hello", "hi", "howdy", "hola" and says hello; intended to
+//     join in on greeting someone when someone else greets them.
 //     Same thing for "goodbye" and "bye"
 
-listen(/PRIVMSG [^ ]+ :(hello|hi|howdy|hola)([!?., ].*)?$/i, function(match, data, replyTo) {
-	irc.privmsg(replyTo, "Hello!");
+var waiting = false;
+function privmsg_throttled(replyTo, msg) {
+    if(!waiting) {
+        irc.privmsg(replyTo, msg);
+        waiting = true;
+        setTimeout(function(){ waiting = false; }, 3000);
+    }
+}
+
+listen(/PRIVMSG [^ ]+ :(hello|hi|howdy|hola|hey)/i, function(match, data, replyTo) {
+    privmsg_throttled(replyTo, "Hello!");
 });
-listen(/PRIVMSG [^ ]+ :(goodbye|bye)/i, function(match, data, replyTo) {
-	irc.privmsg(replyTo, "Goodbye!");
+
+listen(/PRIVMSG [^ ]+ :(goodbye|bye|cya|adios|ttyl)/i, function(match, data, replyTo) {
+    privmsg_throttled(replyTo, "Goodbye!");
 });
