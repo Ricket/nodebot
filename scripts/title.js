b2d4f9e05984b113b36fce7af70b6733a9e9869d
Richard Carter <richard@richardcarter.org>
2012-09-07 13:16:52 -0400

    title: fix missing http/https prefix which is not valid to the request library

--- scripts/title.js
+++ scripts/title.js
@@ -10,6 +10,10 @@ var request = require('request'),
 listen(/\b((?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?]))/i, function(match, data, replyTo) {
     var url = match[0];
 
+    if(url.indexOf('http') != 0) {
+        url = 'https://' + url;
+    }
+
     var requestObject = {
         uri: url,
         strictSSL: false
