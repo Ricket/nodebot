8e86b6c9c11068456493826ef6fed925502a6467
Richard Carter <richard@richardcarter.org>
2013-04-09 13:17:33 -0400

    title: imitate chrome user agent

--- scripts/title.js
+++ scripts/title.js
@@ -16,7 +16,11 @@ listen(/\b((?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]
 
     var requestObject = {
         uri: url,
-        strictSSL: false
+        strictSSL: false,
+        timeout: 10000,
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.43 Safari/537.31 Nodebot'
+        }
     };
 
     request(requestObject, function(error, response, body) {
