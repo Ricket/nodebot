3aa379b5dcd1df6e6651a293be6ee1a442b324f4
Richard Carter <richard@richardcarter.org>
2013-04-11 14:44:29 -0400

    dice: use regexFactory

--- scripts/dice.js
+++ scripts/dice.js
@@ -4,35 +4,46 @@
 // This script handles the following functions:
 //     ~dice|roll [XdY] - rolls a Y-sided die X times, defaults to 1d6; says the result
 
-listen(/~(?:dice|roll)[ ]*$/i, function(match, data, replyTo) {
-    console.log('dice');
-    roll(1, 6, replyTo);
-});
+function printHelp(replyTo) {
+    irc.privmsg(replyTo, "Usage: ~dice [XdY] - where X is number of dice (up "+
+            "to 20) and Y is sides per die (up to 10000)");
+}
 
-listen(/~(?:dice|roll) (.+)$/i, function(match, data, replyTo) {
-    var params;
-    console.log('superdice');
-    try {
-        if (match[1].indexOf('d') > -1) {
-            params = match[1].split('d');
-            roll(parseInt(params[0]), parseInt(params[1]), replyTo);
-            return;
-        }
-    } catch(err) {
-        // fall through to the help message
-    }
-    irc.privmsg(replyTo, "Usage: ~dice [XdY] - where X is number of dice (up to 20) and Y is sides per die");
-});
+function clamp(num, min, max) {
+    return Math.min(Math.max(num, min), max);
+}
 
 function roll(dice, faces, replyTo) {
-    var i = 0, results = [];
-    dice = Math.max(dice, 1);
-    dice = Math.min(dice, 20);
-    faces = Math.max(faces, 1);
-    faces = Math.min(faces, 10000);
+    var i, results = [];
 
-    for(; i < dice; i++) {
-        results.push("" + (Math.floor(Math.random() * faces) + 1));
+    dice = clamp(dice, 1, 20);
+    faces = clamp(faces, 1, 20);
+
+    for(i = 0; i < dice; i++) {
+        results.push( (Math.floor(Math.random() * faces) + 1).toString(10) );
     }
     irc.privmsg(replyTo, results.join(","));
-};
+}
+
+listen(regexFactory.startsWith(['dice','roll']), function(match, data, replyTo) {
+    if (match[1].trim().length === 0) {
+        // If just dice/roll is said, roll a default 1d6
+        roll(1, 6, replyTo);
+        return;
+    }
+
+    var params = match[1].split('d');
+    if (params.length !== 2) {
+        printHelp(replyTo);
+        return;
+    }
+
+    var num1 = parseInt(params[0].trim()),
+        num2 = parseInt(params[1].trim());
+    if (isNaN(num1) || isNaN(num2)) {
+        printHelp(replyTo);
+    } else {
+        roll(num1, num2, replyTo);
+    }
+});
+
