bd87cedef894c3c85e583ce54887909b976f9b54
Richard Carter <richard@richardcarter.org>
2013-04-12 02:00:22 -0400

    rm blankassblank

--- scripts/blankassblank.js
+++
@@ -1,9 +0,0 @@
-// (c) 2011 Richard Carter
-// This code is licensed under the MIT license; see LICENSE.txt for details.
-
-// This script handles the following functions:
-//     When someone says <adjective>-ass <noun>, it repeats the sentence but with the hyphen shifted to the right.
-
-listen(/PRIVMSG [^ ]+ :(.*)([a-zA-Z]+)-ass ([a-zA-Z]+)(.*)$/i, function(match, data, replyTo) {
-    irc.privmsg(replyTo, match[1] + match[2] + " ass-" + match[3] + match[4]);
-});
