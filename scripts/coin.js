822ffb23a693a54e54989cc9456720472f0e5568
Richard Carter <cartr@netapp.com>
2014-01-03 14:44:17 -0500

    coin: add heads and tails commands

--- scripts/coin.js
+++ scripts/coin.js
@@ -1,12 +1,41 @@
-// (c) 2011 Richard Carter
+// (c) 2014 Richard Carter
 // This code is licensed under the MIT license; see LICENSE.txt for details.
 
 // This script handles the following functions:
 //     ~coin|flip - randomly chooses heads or tails
+//     ~heads     - ... and you win if it's heads
+//     ~tails     - ... and you win if it's tails
 
-listen(regexFactory.only(["coin", "flip"]), function(match, data, replyTo) {
+function isHeads(number) {
+    return number < 0.5;
+}
+
+function isTails(number) {
+    return !isHeads(number);
+}
+
+function flipCoin(replyTo, from, expectFunction) {
     irc.action(replyTo, "flips a coin...");
     setTimeout(function() {
-        irc.privmsg(replyTo, (Math.random() < 0.5) ? "Heads!" : "Tails!");
+        var flip = Math.random(),
+            message = from + ": " + (isHeads(flip) ? "Heads!" : "Tails!");
+        
+        if (expectFunction) {
+            message += expectFunction(flip) ? " You win!" : " You lose.";
+        }
+        irc.privmsg(replyTo, message);
     }, 1000);
+}
+
+listen(regexFactory.only(["coin", "flip"]), function(match, data, replyTo, from) {
+    flipCoin(replyTo, from);
 });
+
+listen(regexFactory.only(["heads"]), function(match, data, replyTo, from) {
+    flipCoin(replyTo, from, isHeads);
+});
+
+listen(regexFactory.only(["tails"]), function(match, data, replyTo, from) {
+    flipCoin(replyTo, from, isTails);
+});
+
