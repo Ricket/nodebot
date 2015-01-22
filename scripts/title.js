fcba949adcb23411a600e72ef179cb86f0c2d2db
Richard Carter <richard@richardcarter.org>
2015-01-21 23:44:15 -0500

    title: limit max size, also download only until title is found and then abort

--- scripts/title.js
+++ scripts/title.js
@@ -4,10 +4,18 @@
 // This script handles the following functions:
 //     some url - look up the url's title and announce it
 
-var request = require('request'),
-    entities = require('./lib/entities'),
+var entities = require('./lib/entities'),
     fs = require('fs'),
     exec = require('child_process').exec;
+
+var request = require('request').defaults({
+    strictSSL: false,
+    timeout: 10000,
+    encoding: 'utf8',
+    headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.99 Safari/537.36'
+    }
+});
 
 listen(/PRIVMSG [^ ]+ :.*?\b((?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?]))/i, function(match, data, replyTo) {
     var url = match[1];
@@ -18,95 +26,73 @@ listen(/PRIVMSG [^ ]+ :.*?\b((?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2
 
     console.log('title: Found url: ' + url);
 
-    var requestObject = {
-        uri: url,
-        strictSSL: false,
-        timeout: 10000,
-        encoding: null,
-        headers: {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.43 Safari/537.31 Nodebot'
-        }
-    };
+    var maxSize = 10485760;
 
-    request(requestObject, function(error, response, body) {
-        if(error) {
+    request({url: url, method: 'HEAD'}, function (error, headRes) {
+        if (error) {
             if((""+error).indexOf('SSL routines') < 0) {
                 irc.privmsg(replyTo, "Error looking up URL: " + error);
             }
-        } else if(response.statusCode != 200) {
-            // Ignore 403 Access Forbidden; some websites block bots with this
-            // code (e.g. Wikipedia).
-            if(response.statusCode != 403) {
-                irc.privmsg(replyTo, "" + response.statusCode);
-            }
-        } else if(response.headers['content-type'] && response.headers['content-type'].toLowerCase().indexOf('image') > -1) {
-            var hostname = response.request.host;
-            // Image
-            var tmpFilename = "/tmp/nodebotimg" + (""+Math.random()).substr(2);
+            return;
+        }
 
-            var imgExtensionMatch = /(\.[a-zA-Z0-9]{3,4})$/.exec(url),
-                imgExtension = ".png";
-            if (imgExtensionMatch != null) {
-                imgExtension = imgExtensionMatch[1];
+        var size = headRes.headers['content-length'];
+        if (size && size > maxSize) {
+            console.log('Page size too large: ' + size);
+            return;
+        }
+
+        var req = request({url: url});
+        req.on('error', function (error) {
+            if((""+error).indexOf('SSL routines') < 0) {
+                irc.privmsg(replyTo, "Error looking up URL: " + error);
             }
-           
-            console.log("Writing file " + tmpFilename + imgExtension); 
-            fs.writeFile(tmpFilename + imgExtension, body, { encoding: 'binary' }, function (err1) {
-                if (err1) {
-                    console.error(err1);
-                    return;
+        });
+        var hostname = "";
+        req.on('response', function (response) {
+            hostname = response.request.host;
+
+            var statusCode = response.statusCode;
+            if (statusCode != 200) {
+                req.abort();
+                // Ignore 403 Access Forbidden; some websites block bots with this
+                // code (e.g. Wikipedia).
+                if (statusCode != 403) {
+                    irc.privmsg(replyTo, "" + response.statusCode);
                 }
+            }
+        });
 
-                console.log("Executing tesseract on file");
-                exec('tesseract "' + tmpFilename + imgExtension + '" "' + tmpFilename + '"', { timeout: 90000 }, function (err2, stdout, stderr) {
-                    fs.unlink(tmpFilename + imgExtension);
+        var pageBody = "";
+        req.on('data', function (data) {
+            pageBody += data;
+            if (pageBody.length > maxSize) {
+                console.log('Page size exceeded limit (' + pageBody.length + ')');
+                req.abort();
+            }
 
-                    if (err2) {
-                        console.error(err2);
-                        fs.unlink(tmpFilename + ".txt");
-                        return;
-                    }
-
-                    console.log("Reading tesseract output from " + tmpFilename + ".txt");
-                    fs.readFile(tmpFilename + ".txt", function (err3, data) {
-                        fs.unlink(tmpFilename + ".txt");
-                        if (err3) {
-                            console.err(err3);
-                            return;
-                        }
-
-                        data = irc.sanitize(data.toString())
-                            .replace(/\\r/g, " ").replace(/\\n/g, " ").replace(/^ +| +$/g, "");
-                        if (data.length > 0) {
-                            irc.privmsg(replyTo, hostname + " : " + data.substr(0, (400 - hostname.length)), false);
-                        } else {
-                            irc.privmsg(replyTo, hostname + " : [image, no text found]");
-                        }
-                    });
-                });
-            });
-        } else {
-            var titleMatch = /<title(?: [^>]+)?>([^<]+)<\/title>/i.exec(body.toString("utf8")),
-                hostname = response.request.host;
+            var titleMatch = /<title(?: [^>]+)?>([^<]+)<\/title>/i.exec(pageBody);
 
             if(titleMatch && titleMatch[1]) {
+                req.abort();
+
                 var title = titleMatch[1];
                 
                 // replace multi-spaces/newlines with spaces
-                title = title.replace(/\s{2,}/g," ");
+                title = title.replace(/\s{2,}/g, " ");
                 
                 // trim front and back
-                title = title.replace(/^\s+/,"");
-                title = title.replace(/\s+$/,"");
+                title = title.replace(/^\s+/, "");
+                title = title.replace(/\s+$/, "");
                 
                 // decode HTML entities
                 title = entities.decode(title);
 
                 irc.privmsg(replyTo, hostname + " : " + title, false);
-            } else {
-                irc.privmsg(replyTo, hostname + " : title not found");
             }
-        }
+        });
+
     });
+
 });
 
