b8591384bf580f15915022f16b8faa105d96148d
Richard Carter <richard@richardcarter.org>
2015-02-20 01:09:16 -0500

    emoticons: add shrug

--- scripts/emoticons.js
+++ scripts/emoticons.js
@@ -23,3 +23,7 @@ listen(regexFactory.only("be cool"), function (match, data, replyTo) {
     }, 1500);
 });
 
+listen(regexFactory.only("shrug", "optional"), function (match, data, replyTo) {
+    irc.privmsg(replyTo, "\u00AF\\_(\u30C4)_/\u00AF", false);
+});
+
